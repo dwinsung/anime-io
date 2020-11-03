12fa64379d7238989938df02201232adf5d39124
David Sung <dwinsung@gmail.com>
2020-11-03 12:09:54 -0600

    final edits

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
 
   return (
     <div className="text-white mt-5 mb-2 text-center text-sm font-mono">
-      Copywrite <span className="text-lg">&copy;</span> 2020 | {david} & {chun}
+      Copyright <span className="text-lg">&copy;</span> 2020 | {david} & {chun}
     </div>
   );
 };
